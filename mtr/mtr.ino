d27cb32460a284eb010878be43a068999f7c0b29
Jonathan Licari <jlicari@ryerson.ca>
2020-02-20 17:50:17 -0500

    Update mtr.ino
    Moved manual control functions to manual.h

--- mtr/mtr.ino
+++ mtr/mtr.ino
@@ -8,6 +8,7 @@
 #include <Servo.h>
 #include "mtr.h"
 #include "arm.h"
+#include "manual.h"
 
 void setup() {
   Servo sx, sy;
@@ -58,61 +59,12 @@ void setup() {
 }
 
 void loop() {
-  // Read in state variable
-  mode = analogRead(md);
-
-  // Only run state check if there has been a change in ANALOG voltage
-  // Debouncing
-  if (mode != old && mode != 0) { 
-    // Determine state based on the analog voltage
-    if (mode >= exp0*0.995 && mode <= exp0*1.005)
-      state = 0;
-    else if (mode >= exp1*0.995 && mode <= exp1*1.005)
-      state = 1;
-    else if (mode >= exp2*0.995 && mode <= exp2*1.005)
-      state = 2;
-    else if (mode >= exp3*0.995 && mode <= exp3*1.005)
-      state = 3;
-
-    old = mode;
-  }   
-  
-  // output_var = analogRead(pin_var);
-  joy_x = analogRead(jx);
-  joy_y = analogRead(jy);
-  
-  //rdVolt(); // Print the controls' analog voltage to the Serial monitor
-
-  // Map the 10-bit ADC digital signal to an 8-bit digital signal for the DAC
-  joy_x = map(joy_x, 0, 1023, 0, 255);
-  joy_y = map(joy_y, 0, 1023, 0, 255);
-
-  // Switch case for flight mode selection
-  switch (state) {
-     case 0:
-      Serial.println("Enter Idle State");
-      idle();
-      break;
-
-     case 1:
-      Serial.println("Enter Drive State");
-      drive();
-      break;
-
-     case 2:
-      Serial.println("Enter Precision Movement State");
-      slow();
-      break;
-      
-     case 3:
-      Serial.println("Enter Autonomous State");
-      demo();
-      break;         
+  if (analogRead(cntl) != 1) { // auto
+    demo();
   }
-  
-  // Prints the motor speed to the Serial Monitor
-  //mtrSpeed(); 
-
+  else {
+    manual();
+  }
   //send_data(); // test serial connect before wireless
   // delay(10); Remove unless required for performance
 }
@@ -138,54 +90,6 @@ void set_data() {
   data.x_dat = joy_x;
   data.y_dat = joy_y;
   data.state = state;
-}
-
-void idle() {                         // Do-nothing state
-  /* Do-nothing state
-   * Retains previously set values of motors
-   * Controls can still be manipulated and printed to Serial Monitor
-   * Control input does not affect the motor speed
-   */
-  delay(5000);
-}
-
-void drive() {                       //PWM output
-  // Joystick input
-  if (joy_x < 120) {                    // Left turn
-    if ((128 - joy_y) < 0)
-      analogWrite(ml, 0);
-    else  
-      analogWrite(ml, (128 - joy_y));   // L motor decreases speed
-    analogWrite(mr, (256 - joy_y));     // R motor increases speed
-  }
-  else if (joy_x > 136) {               // Right turn
-    analogWrite(ml, joy_y);             // L motor increases speed
-    analogWrite(mr, (255 - joy_y));     // R motor decreases speed
-  }
-  else {                                // Forward, rest position  
-    analogWrite(ml, joy_y);       
-    analogWrite(mr, joy_y);       
-  }
-  
-  delay(5);
-}
-
-void slow() {
-  /* Precision Movement State is activated when 
-   * img is detected AND within 20cm 
-   */
-   if (joy_x > 136) {     // Right Turn
-    analogWrite(ml, 75);  // L motor increases speed
-    analogWrite(mr, 25);  // R motor decreases speed
-   }
-   else if (joy_x < 120) {// Left Turn
-    analogWrite(ml, 25);  // L motor decreases speed
-    analogWrite(mr, 75);  // R motor increases speed
-   }
-   else { // Forward movement
-    analogWrite(ml, 75);
-    analogWrite(mr, 75);
-   }
 }
 
 int measure() {
@@ -261,22 +165,4 @@ void demo() {
     analogWrite(ml, 140);
     analogWrite(mr, 50);
   }
-}
-
-void rdVolt() { // Print values to serial monitor
-  Serial.print("\nJoystick x-axis position {0,1023} = ");
-  Serial.print(joy_x);
-  Serial.print("\nJoystick y-axis position {0,1023} = ");
-  Serial.print(joy_y);
-  Serial.print("\n\n-----------------");
-}
-
-void mtrSpeed() { // Prints motor speed to serial monitor
-  // Show rotor speed as a percentage of max speed
-  Serial.print("\nMotor operating speed = ");
-  op1 = (joy_y/255.0)*100.00; // FIX *****
-  Serial.print(op1);
-  Serial.print(" %");
-  
-  Serial.print("\n\n-----------------");
 }
