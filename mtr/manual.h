e87987b43cb80d44fa480fa80d4159b267cf67d3
Jonathan Licari <jlicari@ryerson.ca>
2020-03-12 00:29:14 -0400

    Update manual.h

--- mtr/manual.h
+++ mtr/manual.h
@@ -7,63 +7,7 @@
 #ifndef manual_h
 #define manual_h
 
-#include arm.h
-
-void manual() {
-  // Read in state variable
-  mode = analogRead(md);
-
-  // Only run state check if there has been a change in ANALOG voltage
-  // Debouncing
-  if (mode != old && mode != 0) { 
-    // Determine state based on the analog voltage
-    if (mode >= exp0*0.995 && mode <= exp0*1.005)
-      state = 0;
-    else if (mode >= exp1*0.995 && mode <= exp1*1.005)
-      state = 1;
-    else if (mode >= exp2*0.995 && mode <= exp2*1.005)
-      state = 2;
-    else if (mode >= exp3*0.995 && mode <= exp3*1.005)
-      state = 3;
-
-    old = mode;
-  }   
-  
-  //rdVolt(); // Print the controls' analog voltage to the Serial monitor
-
-  // output_var = analogRead(pin_var);
-  joy_x = analogRead(jx);
-  joy_y = analogRead(jy);
-  
-  // Map the 10-bit ADC digital signal to an 8-bit digital signal for the DAC
-  joy_x = map(joy_x, 0, 1023, 0, 255);
-  joy_y = map(joy_y, 0, 1023, 0, 255);
-
-  // Switch case for flight mode selection
-  switch (state) {
-     case 0:
-      Serial.println("Enter Idle State");
-      idle();
-      break;
-
-     case 1:
-      Serial.println("Enter Drive State");
-      drive();
-      break;
-
-     case 2:
-      Serial.println("Enter Precision Movement State");
-      slow();
-      break;
-      
-     case 3:
-      Serial.println("Enter Robotic Arm State");
-      grab();
-  }
-  
-  // Prints the motor speed to the Serial Monitor
-  //mtrSpeed(); 
-}
+#include "mtr.h"
 
 void idle() {                         // Do-nothing state
   /* Do-nothing state
@@ -113,22 +57,8 @@ void slow() {
    }
 }
 
-void rdVolt() { // Print values to serial monitor
-  Serial.print("\nJoystick x-axis position {0,1023} = ");
-  Serial.print(joy_x);
-  Serial.print("\nJoystick y-axis position {0,1023} = ");
-  Serial.print(joy_y);
-  Serial.print("\n\n-----------------");
-}
-
-void mtrSpeed() { // Prints motor speed to serial monitor
-  // Show rotor speed as a percentage of max speed
-  Serial.print("\nMotor operating speed = ");
-  op1 = (joy_y/255.0)*100.00; // FIX *****
-  Serial.print(op1);
-  Serial.print(" %");
+manual_grab() {
   
-  Serial.print("\n\n-----------------");
 }
 
 #endif
