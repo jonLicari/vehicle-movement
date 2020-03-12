8cb7a4102728335fa7ad3a861fa7543bcbf2f0f6
Jonathan Licari <jlicari@ryerson.ca>
2020-03-11 22:18:01 -0400

    Update mtr.h

--- mtr/mtr.h
+++ mtr/mtr.h
@@ -1,60 +1,182 @@
-/* author: jlicari, ELE800, NMK05 - Autonomous Fetching Robot
- *  Created: 10 Feb 2020
- *  mtr.h
- *  This header file contains definitions corresponding to mtr.ino 
+/* author: jlicari, ELE 800, Ryerson University
+ *  Created: 8 February 2020
+ *  mtr.ino
+ *  Vehicle mode selection, sensor assisted navigation, wireless control
  */
-#ifndef mtr_h
-#define mtr_h
+#include <Servo.h> 
+#include "mtr.h"
+#include "manual.h"
 
-  // Define analog constants for the Mode Select debouncing
-  #define exp0 1014 // 1k
-  #define exp1 931 // 10k
-  #define exp2 840 // 22k
-  #define exp3 770 // 33k
-
-  // Pin Assignments
-  const int jx = A0; 
-  const int jy = A1;
-  const int md = A2; // Mode select
-  const int cntl = A3; // 0 - auto, 1 - manual 
-  const int im = 2; // Object detect port
-  const int trig = 4;
-  const int echo = 10;
-  const int grp_cnfm = 11; // Grip signal port
-  const int x = // x-axis servo pin
-  const int y = // y-axis servo pin
+#define ymax  45
+Servo sx, sy;
+void setup() {
+  // Servo Initialization
+  sx.attach(11);
+  sy.attach(12);
+  posx = 0; // Default sx position to 0
+  posy = 0; // Default sy position to 0
+  sx.write(posx);
+  sy.write(posy);    
   
-  // Motor PWM pin declaration
-  const int ml = 3; // L motor
-  const int lfwd = 5; 
-  const int lbwd = 6; 
-  const int mr = 9; // R motor
-  const int rfwd = 8;
-  const int rbwd = 7;
+  // Variable Initialization
+  state = 3; // Default demo state
+  img = false;
+  grp = false;
 
-  // TX Variables
-  const int tx_led = 13;
-  const int tx = 12;
+  // Sensor Port Initialization
+  pinMode(jx, INPUT);
+  pinMode(jy, INPUT);
+  pinMode(md, INPUT);
+  pinMode(im, INPUT_PULLUP);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(grp, INPUT);
 
-  // Analog variables
-  int joy_x = 0;
-  int joy_y = 0;
-  int mode = 0;
+  // Motor Port Initialization
+  pinMode(ml, OUTPUT);
+  pinMode(lfwd, OUTPUT);
+  pinMode(lbwd, OUTPUT);
+  pinMode(mr, OUTPUT);
+  pinMode(rfwd, OUTPUT);
+  pinMode(rbwd, OUTPUT);
 
-  // Miscellaneous 
-  float op1;
-  long dur;  
-  unsigned long bgn, tot_time; // Start/ Stop time 
-  int old, state, dist, posx, posy; 
-  volatile bool img; // Object detect signal 
-  volatile bool grp; // Grip detect signal
+  // Motor Signal Initialization
+  digitalWrite(lfwd, HIGH);
+  digitalWrite(lbwd, LOW);
+  digitalWrite(rfwd, HIGH);
+  digitalWrite(rfwd, LOW);
+  
+  // Tx initialization
+ // vw_set_tx_pin(tx); // Set transmit pin
+ // vw_setup(2000); // Bits per second
+ // pinMode(tx_led, OUTPUT);
 
-struct Data_Packet {
-  int x_dat;
-  int y_dat;
-  int state;
-};
+  // Interrupt 
+  attachInterrupt(digitalPinToInterrupt(im), detect, RISING);
+  attachInterrupt(digitalPinToInterrupt(grp), grip_detect, CHANGE);
 
-Data_Packet data;
+  // Initialize Serial monitor
+  Serial.begin(250000);
+}
 
-#endif
+void loop() {
+  if (analogRead(cntl) != 1) { // auto
+    demo();
+  }
+  else {
+    manual();
+  }
+  //send_data(); // test serial connect before wireless
+  // delay(10); Remove unless required for performance
+}
+
+void detect() {
+  img = !img;
+}
+
+void grip_detect() {
+  grp = !grp;
+}
+
+/*void send_data() {
+  digitalWrite(tx_led, HIGH); // Begin transmit
+  set_data(); // Substitute for reading values
+  vw_send((uint8_t *)&data, sizeof(data));
+  vw_wait_tx(); // Wait until the whole message sends
+  digitalWrite(tx_led, LOW);
+  // delay(5);
+}
+
+void set_data() {
+  data.x_dat = joy_x;
+  data.y_dat = joy_y;
+  data.state = state;
+}*/
+
+int measure() {
+  int i;
+  for (i = 0; i < 2; i++) {
+    // Clear trig
+    digitalWrite(trig, LOW);
+    delayMicroseconds(2);
+
+    // Set trig HIGH for 10 ms
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+
+    // Read echo and return travel time in ms
+    dur = pulseIn(echo, HIGH);
+
+    // Calculate distance
+    dist += dur*0.034/2;
+  }
+  dist = dist/i;
+  Serial.println("Distance: ");
+  Serial.print(dist);
+
+  return(dist); 
+}
+
+void deliver(unsigned long time) {
+  // 180 degree turn
+  analogWrite(ml, 255);
+  analogWrite(mr, 200);
+  delay(2000);
+  
+  // Return
+  analogWrite(ml, 200);
+  analogWrite(mr, 200);
+  delay(time); 
+} 
+
+void grab() {
+  // sx operation
+  while(grp != true) { // or digitalRead(pin12) != HIGH
+    sx.write(posx);
+    posx++;
+  }
+  
+  // sy operation
+  for (posy = 0; posy < ymax; posy++) {
+    sy.write(posy);
+  }
+}
+
+void demo() {
+  int spd, d;
+  bgn = millis();
+    
+  while(img != false) {
+    Serial.println("true loop ");
+
+    d = measure();
+    Serial.println("d = ");
+    Serial.println(d);
+    
+    if (d > 20) { // Distance > 20cm from object
+      spd = 255;
+      analogWrite(ml, spd);
+      analogWrite(mr, spd);
+    }
+    else if (d > 8 && d < 20) { // Slow: 20cm > Distance > 8cm
+      spd = 175;
+      analogWrite(ml, spd);
+      analogWrite(mr, spd);
+    }
+    else { // Stop: Distance <= 7cm
+      tot_time = millis() - bgn;
+      analogWrite(ml, 0);
+      analogWrite(mr, 0);
+      grab(); // See arm.h
+      deliver(tot_time); // Delivers object to requester
+    }
+  }
+  
+  // object not detected
+  if (img != true) {
+    Serial.println("false");
+    analogWrite(ml, 140);
+    analogWrite(mr, 50);
+  }
+}
